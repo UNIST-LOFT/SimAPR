333aabf72f6f2cc54123fed2678623a8369db731
hsh814 <hsh814@naver.com>
2021-12-23 17:04:40 +0900

    Fix select_by_probability_hierarchical, apply fl in guided

--- core.py
+++ core.py
@@ -70,12 +70,9 @@ class PassFail:
   def expect_probability(self,additional_score:float=0) -> float:
     return self.beta_mode(self.pass_count + 1.5+additional_score, self.fail_count + 2.0)
   @staticmethod
-  def select_by_probability(pf_list: list,additional_score:List[float]=None) -> int:   # pf_list: list of PassFail
+  def select_by_probability(pf_list: list) -> int:   # pf_list: list of PassFail
     probability=[]
-    if additional_score is None:
-      probability = list(map(lambda x: x.expect_probability(), pf_list))
-    else:
-      probability = list(map(lambda x,score: x.expect_probability(score), pf_list,additional_score))
+    probability = list(map(lambda x: x.expect_probability(), pf_list))
     total = sum(probability)
     rand = random.random() * total
     for i in range(len(pf_list)):
