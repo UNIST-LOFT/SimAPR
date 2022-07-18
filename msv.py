8b67aee6cf3743a3c14f4f86204fbd946b0f4274
FreddyYJ <kyj1411@unist.ac.kr>
2022-07-18 02:35:04 -0700

    Fix iteration counter

--- msv.py
+++ msv.py
@@ -443,7 +443,7 @@ class MSVTbar(MSV):
         if result and self.state.use_pass_test:
           pass_result,pass_time = self.run_test_positive(patch)
           result_handler.update_positive_result_tbar(self.state, patch, pass_result)
-      self.state.iteration += 1
+        self.state.iteration += 1
       result_handler.append_result(self.state, [patch], pass_exists, pass_result, result, is_compilable,fail_time,pass_time)
       result_handler.remove_patch_tbar(self.state, patch)
   
@@ -491,7 +491,7 @@ class MSVTbar(MSV):
           result_handler.update_result_tbar(self.state, patch, pass_exists)
           if result:
             result_handler.update_positive_result_tbar(self.state, patch, pass_result)
-      self.state.iteration += 1
+        self.state.iteration += 1
       result_handler.append_result(self.state, [patch], pass_exists, pass_result, result, is_compilable,fail_time,pass_time)
       result_handler.remove_patch_tbar(self.state, patch)
 
