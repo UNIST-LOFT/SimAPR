0c0186f73c8d0aa0facbff741e1c80c879052f31
FreddyYJ <kyj1411@unist.ac.kr>
2022-10-07 14:51:51 +0900

    Update condition synthesizer to use more conditions

--- condition.py
+++ condition.py
@@ -401,6 +401,8 @@ class ProphetCondition:
     for test in values:
       # collect all available constants
       for i,var in enumerate(test):
+        if self.state.use_msv_ext:
+          available_const[i].add(0)  # Always use 0 since it's usually used by pointers, but not for original prophet/SPR
         if self.state.use_fixed_const:
           # use fixed constant(-100 ≤ c ≤ 100) instead of constants from test execution
           for j in range(-100,101):
@@ -423,12 +425,22 @@ class ProphetCondition:
           if result>=0:
             init_results.append(self.SynthesisResult(i,value,oper,result))
 
-    sorted_result=sorted(init_results)[:20]
-    for result in sorted_result:
+    sorted_result=sorted(init_results)
+    new_sorted=[]
+    if self.state.use_msv_ext:
+      if sorted_result[19].result==0: # If result==0 conditions are more than 20, use every conditions
+        for res in sorted_result:
+          if res.result==0:
+            new_sorted.append(res)
+      else:
+        new_sorted=sorted_result[:20]
+    else:
+      new_sorted=sorted_result[:20]
+    for result in new_sorted:
       self.state.msv_logger.info(f"Created {result.result}: {result.operator}-{result.var1}-{result.const_or_var2_value}")
 
     final_result=[]
-    for result in sorted_result:
+    for result in new_sorted:
       if OperatorType.EQ_VAR <= result.operator <= OperatorType.LT_VAR:
         final_result.append((result.operator,result.var1,result.var2))
       else:
@@ -906,6 +918,8 @@ class GuidedPathCondition:
     for test in values:
       # collect all available constants
       for i,var in enumerate(test):
+        if self.state.use_msv_ext:
+          available_const[i].add(0)  # Always use 0 since it's usually used by pointers, but not for original prophet/SPR
         if self.state.use_fixed_const:
           # use fixed constant(-100 ≤ c ≤ 100) instead of constants from test execution
           for j in range(-100,101):
@@ -928,12 +942,22 @@ class GuidedPathCondition:
           if result>=0:
             init_results.append(self.SynthesisResult(i,value,oper,result))
 
-    sorted_result=sorted(init_results)[:20]
-    for result in sorted_result:
+    sorted_result=sorted(init_results)
+    new_sorted=[]
+    if self.state.use_msv_ext:
+      if sorted_result[19].result==0: # If result==0 conditions are more than 20, use every conditions
+        for res in sorted_result:
+          if res.result==0:
+            new_sorted.append(res)
+      else:
+        new_sorted=sorted_result[:20]
+    else:
+      new_sorted=sorted_result[:20]
+    for result in new_sorted:
       self.state.msv_logger.info(f"Created {result.result}: {result.operator}-{result.var1}-{result.const_or_var2_value}")
 
     final_result=[]
-    for result in sorted_result:
+    for result in new_sorted:
       if OperatorType.EQ_VAR <= result.operator <= OperatorType.LT_VAR:
         final_result.append((result.operator,result.var1,result.var2))
       else:
