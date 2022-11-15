c5e05de271bad507f30c1ed7116cf773600a670e
foodsnow <dut.kht@gmail.com>
2022-11-15 11:57:38 +0900

    Fix fixminer mode

--- msv.py
+++ msv.py
@@ -372,11 +372,7 @@ class MSVTbar(MSV):
     elif self.state.time_limit > 0 and (self.state.select_time+self.state.test_time) > self.state.time_limit:
       self.state.is_alive = False
     elif len(self.state.patch_ranking) == 0:
-      if self.state.fixminer_mode and not self.state.fixminer_swapped:
-        self.state.msv_logger.info('First group searched, swap to second group')
-        self.state.fixminer_swap_info()
-      else:
-        self.state.is_alive = False
+      self.state.is_alive = False
     elif self.state.finish_at_correct_patch and self.patch_str in self.state.correct_patch_str:
       self.state.is_alive = False
     return self.state.is_alive
