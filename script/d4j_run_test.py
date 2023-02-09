3d25ac9c698e0605e9f620450849ad26b07eb310
hsh814 <hsh814@naver.com>
2023-02-09 04:58:52 +0000

    Update d4j_run_test.py: Support d4j v2

--- script/d4j_run_test.py
+++ script/d4j_run_test.py
@@ -10,7 +10,22 @@ from pathlib import Path
 from psutil import Popen
 import psutil
 
-def get_paths(project):
+def run_d4j_export(d4j_dir: str) -> tuple:
+  class_dir_file = d4j_dir + "/bin.classes"
+  test_dir_file = d4j_dir + "/bin.tests"
+  if not os.path.exists(class_dir_file):
+    cmd = ["defects4j", "export", "-p", "dir.bin.classes", "-w", d4j_dir, "-o", class_dir_file]
+    subprocess.run(cmd, check=True)
+  if not os.path.exists(test_dir_file):
+    cmd = ["defects4j", "export", "-p", "dir.bin.tests", "-w", d4j_dir, "-o", test_dir_file]
+    subprocess.run(cmd, check=True)
+  if not os.path.exists(class_dir_file) or not os.path.exists(test_dir_file):
+    raise Exception("d4j export failed")
+  class_dir = open(class_dir_file).read().strip()
+  test_dir = open(test_dir_file).read().strip()
+  return class_dir, test_dir
+
+def get_paths(project, buggy_dir):
   sep = "_"
   if "MSV_RECODER" in os.environ:
     sep = os.environ["MSV_RECODER"]
@@ -37,16 +52,16 @@ def get_paths(project):
     if 11 <= bug_id or 18 <= bug_id <= 21:
       return "/build/classes/main/", "/build/classes/test/"
     return "/target/classes/", "/target/test-classes/"
-  return None, None
+  return run_d4j_export(buggy_dir)
 
 def get_classpath(work_dir, buggy_project):
   # Todo for all defects4j projects
-  classpath, _ = get_paths(buggy_project)
+  classpath, _ = get_paths(buggy_project, work_dir)
   return work_dir + classpath
 
 def get_test_classpath(work_dir, buggy_project):
   # Todo for all defects4j projects
-  _, test_classpath = get_paths(buggy_project)
+  _, test_classpath = get_paths(buggy_project, work_dir)
   return work_dir + test_classpath
 
 def junit_classpath(tbar):
