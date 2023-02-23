5e6f30fdc38c8b100895a748e9df72cb699bf210
freddyyj <kyj1411@unist.ac.kr>
2023-02-23 10:23:30 +0900

    Update scripts

--- experiments/avatar/avatar.py
+++ experiments/avatar/avatar.py
@@ -3,30 +3,30 @@ import subprocess
 import sys
 import os
 
-def run(project):
+def run(project,defects4j_path):
     os.chdir('../../Avatar')
     print(f"Run {project}")
-    result=subprocess.run(['defects4j','checkout','-p',project.split('_')[0],'-v',project.split('_')[1]+'b',
+    result=subprocess.run([defects4j_path,'checkout','-p',project.split('_')[0],'-v',project.split('_')[1]+'b',
                         '-w',f'buggy/{project}'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     if result.returncode!=0:
         print(result.stdout.decode('utf-8'))
         print(f'{project} checkout failed with return code {result.returncode}')
         return
     
-    result=subprocess.run(['defects4j','compile','-w',f'buggy/{project}'],
+    result=subprocess.run([defects4j_path,'compile','-w',f'buggy/{project}'],
                     stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     if result.returncode!=0:
         print(result.stdout.decode('utf-8'))
         print(f'{project} checkout failed with return code {result.returncode}')
         return
 
-    result=subprocess.run(['defects4j','test','-w',f'buggy/{project}'],
+    result=subprocess.run([defects4j_path,'test','-w',f'buggy/{project}'],
                     stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     with open(f'FailedTestCases/{project}.txt','w') as f:
         f.write(result.stdout.decode('utf-8'))
     
-    result=subprocess.run(['./FLFix.sh',project
-            ],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    result=subprocess.run(['java','-Xmx100g','-cp','target/dependency/*','edu.lu.uni.serval.main.Main',
+            'buggy/',defects4j_path+'/',project,'Ochiai'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     output=result.stdout.decode('utf-8')
     print(output)
     print(f'{project} finish with return code {result.returncode}')
@@ -34,7 +34,7 @@ def run(project):
 if __name__ == '__main__':
     args=sys.argv
     if len(args)!=2:
-        print("Usage: python3 avatar.py <project>")
+        print("Usage: python3 avatar.py <project> <defectsj4_path>")
         exit(1)
     
-    run(args[1])
+    run(args[1],args[2])
--- experiments/fixminer/fixminer.py
+++ experiments/fixminer/fixminer.py
@@ -3,24 +3,24 @@ import subprocess
 import sys
 import os
 
-def run(project):
+def run(project,defects4j_path):
     os.chdir('../../Fixminer')
     print(f"Run {project}")
-    result=subprocess.run(['defects4j','checkout','-p',project.split('_')[0],'-v',project.split('_')[1]+'b',
+    result=subprocess.run([defects4j_path,'checkout','-p',project.split('_')[0],'-v',project.split('_')[1]+'b',
                         '-w',f'buggy/{project}'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     if result.returncode!=0:
         print(result.stdout.decode('utf-8'))
         print(f'{project} checkout failed with return code {result.returncode}')
         return
     
-    result=subprocess.run(['defects4j','compile','-w',f'buggy/{project}'],
+    result=subprocess.run([defects4j_path,'compile','-w',f'buggy/{project}'],
                     stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     if result.returncode!=0:
         print(result.stdout.decode('utf-8'))
         print(f'{project} checkout failed with return code {result.returncode}')
         return
 
-    result=subprocess.run(['defects4j','test','-w',f'buggy/{project}'],
+    result=subprocess.run([defects4j_path,'test','-w',f'buggy/{project}'],
                     stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     with open(f'FailedTestCases/{project}.txt','w') as f:
         f.write(result.stdout.decode('utf-8'))
@@ -28,7 +28,7 @@ def run(project):
     result=subprocess.run(['java','-Xmx100G','-cp', 'lib/JavaCodeParser-1.0.jar:target/FixMiner-0.0.1-SNAPSHOT-jar-with-dependencies.jar',
                 'edu.lu.uni.serval.fixminer.main.Main',
                 'FailedTestCases/','BugPositions/','buggy/',
-                '/defects4j/',project],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+                defects4j_path+'/',project],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     output=result.stdout.decode('utf-8')
     print(output)
     print(f'{project} finish with return code {result.returncode}')
@@ -36,7 +36,7 @@ def run(project):
 if __name__ == '__main__':
     args=sys.argv
     if len(args)!=2:
-        print("Usage: python3 fixminer.py <project>")
+        print("Usage: python3 fixminer.py <project> <defectsj4_path>")
         exit(1)
     
-    run(args[1])
+    run(args[1],args[2])
