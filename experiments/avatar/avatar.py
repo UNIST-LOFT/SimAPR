f1a98ef894cc05c55879ba8263e4c65d2f19a73d
freddyyj <kyj1411@unist.ac.kr>
2023-02-23 16:35:44 +0900

    Fix avatar script

--- experiments/avatar/avatar.py
+++ experiments/avatar/avatar.py
@@ -9,7 +9,7 @@ def run(project):
     if not os.path.exists('d4j'):
         os.mkdir('d4j')
     print(f"Run {project}")
-    result=subprocess.run(['defects4j'+'/framework/bin/defects4j','checkout','-p',project.split('_')[0],'-v',project.split('_')[1]+'b',
+    result=subprocess.run(['defects4j','checkout','-p',project.split('_')[0],'-v',project.split('_')[1]+'b',
                         '-w',f'buggy/{project}'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     if result.returncode!=0:
         print(result.stdout.decode('utf-8'))
