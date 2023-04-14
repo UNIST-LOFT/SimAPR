9a895881dfb3a478fe8da185a325342f358c9120
FreddyYJ <kyj1411@unist.ac.kr>
2023-04-14 06:16:01 +0000

    Fix original algo option

--- experiments/alpharepair/search-alpharepair-orig.py
+++ experiments/alpharepair/search-alpharepair-orig.py
@@ -17,7 +17,7 @@ def run(project):
         new_cur_dir+=dir+'/'
 
     print(f"Run {project}-original")
-    result=subprocess.run(['python3',f'{new_cur_dir}/SimAPR/simapr.py','-o',f'result/{project}-orig','-m','recoder',
+    result=subprocess.run(['python3',f'{new_cur_dir}/SimAPR/simapr.py','-o',f'result/{project}-orig','-m','orig',
                 '-k','learning','-w',f'{new_cur_dir}/AlphaRepair/d4j/{project}','-t','180000','--use-simulation-mode',f'result/cache/{project}-cache.json',
                 '-T','18000', '--','python3',
                 f'{new_cur_dir}/SimAPR/script/d4j_run_test.py',f'{new_cur_dir}/AlphaRepair/buggy'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
