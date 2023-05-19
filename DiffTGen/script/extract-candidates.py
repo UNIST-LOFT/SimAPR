ac14c5f5a12c9305e4b5a82624a42acf8a37e9e2
FreddyYJ <kyj1411@unist.ac.kr>
2023-05-19 02:49:42 +0000

    Update template based for difftgen

--- DiffTGen/script/extract-candidates.py
+++ DiffTGen/script/extract-candidates.py
@@ -141,50 +141,58 @@ def main_recoder(cmd: str, rootdir: str, outdir: str) -> None:
         os.system(f"cp {original} {target}")
 
 def main_tbar(rootdir,outdir,tool):
-  for bugid in os.listdir(os.path.join(rootdir, "d4j")):
-    print(bugid)
-    bugid_recoder = bugid.replace("_", "-")
-    os.makedirs(f"{outdir}/{bugid_recoder}", exist_ok=True)
-    if tool=='fixminer':
-      switch_info_file = os.path.join(rootdir, "d4j", bugid, "0","switch-info.json")
-    else:
-      switch_info_file = os.path.join(rootdir, "d4j", bugid, "switch-info.json")
-    sim_file = os.path.join(rootdir, "../experiment", f".cache-{tool}", f"{bugid}-cache.json")
+  tool_dir = ""
+  sim_dir = os.path.join(rootdir, "experiments", cmd, "result", "cache")
+  if cmd == "tbar":
+    tool_dir = os.path.join(rootdir, "TBar")
+  elif cmd == "avatar":
+    tool_dir = os.path.join(rootdir, "Avatar")
+  elif cmd == "kpar":
+    tool_dir = os.path.join(rootdir, "kPar")
+  elif cmd == "fixminer":
+    tool_dir = os.path.join(rootdir, "FixMiner")
+
+  bugids = get_bugids(f"{tool_dir}/data/bugs.csv")
+  for bugid in bugids:
+    switch_info_file = os.path.join(tool_dir, "d4j", bugid, "switch-info.json")
+    sim_file = os.path.join(sim_dir, f"{bugid}-cache.json")
     if not os.path.exists(sim_file):
-      print(f"SKIP {bugid} - {sim_file} not exists")
       continue
+    print(f"Processing {bugid}")
+    
     with open(switch_info_file, "r") as swf, open(sim_file, "r") as sf:
       sw = json.load(swf)
       sim = json.load(sf)
       plau_list = get_plausible(sim)
       result = dict()
-      result["bugid"] = bugid_recoder
-      result["tool"] = tool
+      result["bugid"] = sw["project_name"]
+      result["tool"] = "tbar"
       result["correct_patch"] = dict()
       plau_patches = list()
       result["plausible_patches"] = plau_patches
+
       for file_info in sw["rules"]:
         file_name = file_info["file_name"]
         for line_info in file_info["lines"]:
-          if "switches" in line_info:
-            patches=line_info['switches']
+          if 'cases' in line_info:
+            cases=line_info["cases"]
           else:
-            patches=line_info['cases']
-          for case_info in patches:
+            cases=line_info["switches"]
+
+          for case_info in cases:
             loc = case_info["location"]
-            tokens = loc.split("/")
-            id = loc.replace(tokens[-1], "")
-            id = id.replace("/", "_")
-            patch_info = {"id": id, "location": loc, "file": file_name, "line": line_info["line"]}
+            patch_info = {"id": loc, "location": loc, "file": file_name, "line": line_info["line"]}
             if loc in plau_list:
               plau_patches.append(patch_info)
-      with open(f"{outdir}/{bugid_recoder}/{bugid_recoder}.json", "w") as f:
+      if len(plau_list) == 0:
+        continue
+      print(bugid)
+      os.makedirs(f"{outdir}/{bugid}", exist_ok=True)
+      with open(f"{outdir}/{bugid}/{bugid}.json", "w") as f:
         json.dump(result, f, indent=2)
-      if len(plau_patches) > 0:
-        print(f"{bugid}: found {len(plau_patches)} plausible patches")
       for plau in plau_list:
-        original = os.path.join(rootdir, "d4j", bugid) + "/" + plau
-        target = os.path.join(outdir, bugid_recoder) + "/" + plau
+        original = os.path.join(tool_dir, "d4j", bugid, plau)
+        target = os.path.join(outdir, bugid, plau)
         os.makedirs(os.path.dirname(target), exist_ok=True)
         os.system(f"cp {original} {target}")
 
