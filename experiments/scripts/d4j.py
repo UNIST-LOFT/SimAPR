d1f57ddc3cbf9e7b351b6609a584c19b42fab1cb
FreddyYJ <kyj1411@unist.ac.kr>
2023-05-19 05:09:49 +0000

    Add plot scripts

---
+++ experiments/scripts/d4j.py
@@ -0,0 +1,73 @@
+CHART_SIZE=26
+CLOSURE_SIZE=133
+LANG_SIZE=65
+MATH_SIZE=106
+MOCKITO_SIZE=38
+TIME_SIZE=27
+
+MOCKITO_SKIP=(9,10,11,21)
+
+CLI_SIZE=40
+CODEC_SIZE=18
+COLLECTIONS_SIZE=28
+COMPRESS_SIZE=47
+CSV_SIZE=16
+GSON_SIZE=18
+JACKSON_CORE_SIZE=26
+JACKSON_DATABIND_SIZE=112
+JACKSON_XML_SIZE=6
+JSOUP_SIZE=93
+JXPATH_SIZE=22
+CLOSURE_NEW=[i for i in range(133,175)]
+
+CLI_SKIP=(6,)
+COLLECTIONS_SKIP=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)
+JACKSON_DATABIND_SKIP=[]
+for i in range(58,113):
+    JACKSON_DATABIND_SKIP.append(i)
+
+__temp_list=[]
+for i in range(1,CHART_SIZE+1):
+    __temp_list.append(f'Chart_{i}',)
+for i in range(1,CLOSURE_SIZE+1):
+    __temp_list.append(f'Closure_{i}',)
+for i in range(1,LANG_SIZE+1):
+    __temp_list.append(f'Lang_{i}',)
+for i in range(1,MATH_SIZE+1):
+    __temp_list.append(f'Math_{i}',)
+for i in range(1,MOCKITO_SIZE+1):
+    if i in MOCKITO_SKIP: continue
+    __temp_list.append(f'Mockito_{i}',)
+for i in range(1,TIME_SIZE+1):
+    __temp_list.append(f'Time_{i}',)
+D4J_1_2_LIST=tuple(__temp_list)
+
+__temp_list=[]
+for i in range(1,CLI_SIZE+1):
+    if i in CLI_SKIP: continue
+    __temp_list.append(f'Cli_{i}',)
+for i in range(1,CODEC_SIZE+1):
+    __temp_list.append(f'Codec_{i}',)
+for i in range(1,COLLECTIONS_SIZE+1):
+    if i in COLLECTIONS_SKIP: continue
+    __temp_list.append(f'Collections_{i}',)
+for i in range(1,COMPRESS_SIZE+1):
+    __temp_list.append(f'Compress_{i}',)
+for i in range(1,CSV_SIZE+1):
+    __temp_list.append(f'Csv_{i}',)
+for i in range(1,GSON_SIZE+1):
+    __temp_list.append(f'Gson_{i}',)
+for i in range(1,JACKSON_CORE_SIZE+1):
+    __temp_list.append(f'JacksonCore_{i}',)
+for i in range(1,JACKSON_DATABIND_SIZE+1):
+    if i in JACKSON_DATABIND_SKIP: continue
+    __temp_list.append(f'JacksonDatabind_{i}',)
+for i in range(1,JACKSON_XML_SIZE+1):
+    __temp_list.append(f'JacksonXml_{i}',)
+for i in range(1,JSOUP_SIZE+1):
+    __temp_list.append(f'Jsoup_{i}',)
+for i in range(1,JXPATH_SIZE+1):
+    __temp_list.append(f'JxPath_{i}',)
+for i in range(1,CLOSURE_NEW+1):
+    __temp_list.append(f'Closure_{i}',)
+D4J_2_LIST=tuple(__temp_list)
---
+++ experiments/scripts/rq3.py
@@ -0,0 +1,198 @@
+import os
+from typing import Dict, List
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn
+
+import d4j
+
+orig_result:List[int]=[]
+wo_vertical:List[List[int]]=[[] for _ in range(50)]
+wo_horizontal:List[List[int]]=[[] for _ in range(50)]
+casino_result:List[List[int]]=[[] for _ in range(50)]
+
+def plot_patches_ci_java(mode='tbar'):
+    # Casino
+    for i in range(50):
+        for result in d4j.D4J_1_2_LIST:
+            try:
+                result_file=open(f'{mode}/result/{result}-casino-{i}/simapr-result.json','r')
+            except:
+                continue
+            root=json.load(result_file)
+            result_file.close()
+
+            prev_time=0.
+            for res in root:
+                is_hq=res['result']
+                is_plausible=res['pass_result']
+                iteration=res['iteration']
+                time=res['time']
+                loc=res['config'][0]['location']
+
+                if is_plausible:
+                    casino_result[i].append(round((time)/60))
+
+                if time>3600:
+                    break
+
+    # w/o vertical
+    for i in range(50):
+        for result in d4j.D4J_1_2_LIST:
+            try:
+                result_file=open(f'{mode}/result/{result}-wo-vertical-{i}/simapr-result.json','r')
+            except:
+                continue
+            root=json.load(result_file)
+            result_file.close()
+
+            prev_time=0.
+            for res in root:
+                is_hq=res['result']
+                is_plausible=res['pass_result']
+                iteration=res['iteration']
+                time=res['time']
+                loc=res['config'][0]['location']
+
+                if is_plausible:
+                    wo_vertical[i].append(round((time)/60))
+
+                if time>3600:
+                    break
+
+    # w/o horizontal
+    for i in range(50):
+        for result in d4j.D4J_1_2_LIST:
+            try:
+                result_file=open(f'{mode}/result/{result}-wo-horizontal-{i}/simapr-result.json','r')
+            except:
+                continue
+            root=json.load(result_file)
+            result_file.close()
+
+            prev_time=0.
+            for res in root:
+                is_hq=res['result']
+                is_plausible=res['pass_result']
+                iteration=res['iteration']
+                time=res['time']
+                loc=res['config'][0]['location']
+
+                if is_plausible:
+                    wo_horizontal[i].append(round((time)/60))
+
+                if time>3600:
+                    break
+
+    # Original
+    for result in d4j.D4J_1_2_LIST:
+        try:
+            result_file=open(f'{mode}/result/{result}-orig/simapr-result.json','r')
+        except:
+            continue
+        root=json.load(result_file)
+        result_file.close()
+
+        prev_time=0.
+        for res in root:
+            is_hq=res['result']
+            is_plausible=res['pass_result']
+            iteration=res['iteration']
+            time=res['time']
+            loc=res['config'][0]['location']
+
+            if is_plausible:
+                orig_result.append(round((time)/60))
+
+            if time>3600:
+                break
+
+plot_patches_ci_java('tbar')
+plot_patches_ci_java('avatar')
+plot_patches_ci_java('kpar')
+plot_patches_ci_java('fixminer')
+
+# Plausible patch plot
+plt.clf()
+fig=plt.figure(figsize=(4,3))
+
+# Original
+results=sorted(orig_result)
+other_list=[0]
+for i in range(0,300):
+    if i in results:
+        other_list.append(other_list[-1]+results.count(i))
+    else:
+        other_list.append(other_list[-1])
+plt.plot(list(range(0,301)),other_list,'-.b',label='Orig')
+
+# Casino
+guided_list:List[List[int]]=[]
+guided_x=[]
+guided_y=[]
+temp_=[[],[],[],[],[]]
+for j in range(50):
+    cur_result=sorted(casino_result[j])
+    guided_list.append([0])
+    for i in range(0,300):
+        if i in cur_result:
+            guided_list[-1].append((50*guided_list[-1][-1]+cur_result.count(i))/50)
+            guided_x.append(i)
+            guided_y.append((50*guided_list[-1][-1]+cur_result.count(i))/50)
+        else:
+            guided_list[-1].append(guided_list[-1][-1])
+            guided_x.append(i)
+            guided_y.append(guided_list[-1][-1])
+        if i%60==0:
+            temp_[i//60].append(guided_list[-1][-1])
+guided_df=pd.DataFrame({'Time':guided_x,'Number of valid patches':guided_y})
+seaborn.lineplot(data=guided_df,x='Time',y='Number of valid patches',color='r',label='Casino')
+for i in range(5):
+    print(f'{i*60}: {np.std(temp_[i])}')
+
+# w/o vertical
+guided_list:List[List[int]]=[]
+guided_x=[]
+guided_y=[]
+for j in range(50):
+    cur_result=sorted(wo_vertical[j])
+    guided_list.append([0])
+    for i in range(0,300):
+        if i in cur_result:
+            guided_list[-1].append((50*guided_list[-1][-1]+cur_result.count(i))/50)
+            guided_x.append(i)
+            guided_y.append((50*guided_list[-1][-1]+cur_result.count(i))/50)
+        else:
+            guided_list[-1].append(guided_list[-1][-1])
+            guided_x.append(i)
+            guided_y.append(guided_list[-1][-1])
+guided_df=pd.DataFrame({'Time':guided_x,'Number of valid patches':guided_y})
+seaborn.lineplot(data=guided_df,x='Time',y='Number of valid patches',color='g',label='w/o vertical')
+
+# wo horizontal
+other_list:List[List[int]]=[]
+other_x=[]
+other_y=[]
+for j in range(50):
+    cur_result=sorted(wo_horizontal[j])
+    other_list.append([0])
+    for i in range(0,300):
+        if i in cur_result:
+            other_list[-1].append((50*other_list[-1][-1]+cur_result.count(i))/50)
+            other_x.append(i)
+            other_y.append((50*other_list[-1][-1]+cur_result.count(i))/50)
+        else:
+            other_list[-1].append(other_list[-1][-1])
+            other_x.append(i)
+            other_y.append(other_list[-1][-1])
+other_df=pd.DataFrame({'Time':other_x,'Number of valid patches':other_y})
+seaborn.lineplot(data=other_df,x='Time',y='Number of valid patches',color='y',label='w/o horizontal',linestyle='dashed')
+
+plt.legend(fontsize=12)
+plt.xlabel('Time (min)',fontsize=15)
+plt.ylabel('# of Valid Patches',fontsize=15)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.savefig(f'rq3.pdf',bbox_inches='tight')
